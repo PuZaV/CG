aa5f52e1e9c7719edd0a5ac07d67df6557c866e4
PuZaV924 <87533640+PuZaV@users.noreply.github.com>
2022-08-23 18:48:56 +0545

    Add files via upload

---
+++ bresenham.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <graphics.h>
+#include<dos.h>
+void drawline(int x0, int y0, int x1, int y1)
+{
+    int dx, dy, p, x, y;
+
+    dx = x1 - x0;
+    dy = y1 - y0;
+    x = x0;
+    y = y0;
+    p = 2 * dy - dx;
+    while (x < x1)
+    {
+    	 putpixel(x, y, 15);
+        if (p >= 0)
+        {
+            y = y + 1;
+            p = p + 2 * dy - 2 * dx;
+        }
+        else
+        {
+            p = p + 2 * dy;
+        }
+        x = x + 1;
+    }
+}
+
+int main()
+{
+    int gdriver = DETECT, gmode,  x0, y0, x1, y1;
+    initgraph(&gdriver, &gmode, (char*)"");
+
+    printf("Enter co-ordinates of first point: ");
+    scanf("%d%d", &x0, &y0);
+
+    printf("Enter co-ordinates of second point: ");
+    scanf("%d%d", &x1, &y1);
+    drawline(x0, y0, x1, y1);
+    getch();
+    return 0;
+}
